2e904ab81a40ad591fb7e1a1c7c14b926036b763
Pierre Vandenberghe <pierre@evoc.ca>
2017-05-10 17:38:41 -0400

    added route to bixi controller and url to bixi xml file in geolocation initializer

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :bixis
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource :geocoder
+  root "bixis#index"
 end
